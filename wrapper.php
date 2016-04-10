3900f7a02b7e4a82fd25cc15c7c868685ba727a0
bootjp <oh@bootjp.me>
2016-04-10 23:12:02 +0900

    modify / remove white space

--- wrapper.php
+++ wrapper.php
@@ -6,7 +6,7 @@ require_once (__DIR__ . '/Checker.php');
 $iniFilePath = getenv('ENV_INI');
 $checker = new Checker($iniFilePath !== false ? $iniFilePath : null);
 $result = $checker->fetch();
-    
+
 if (count($result) !== 0) {
     var_export($result);
     $checker->getException();
